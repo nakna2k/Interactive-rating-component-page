6dd2105ffa22833d112688e78c8b51b193ba097b
Wiktor Wyroba <wswyroba@gmail.com>
2022-07-30 23:41:10 +0200

    testing script

---
+++ script.js
@@ -0,0 +1,10 @@
+const SUBMIT_BTN = document.querySelector("#submitBtn");
+const INPUT_RADIO = document.getElementsByName("rating");
+
+
+
+
+SUBMIT_BTN.addEventListener("click",() =>{
+    console.log(INPUT_RADIO.length);
+    }
+);
